7bf2e5a8a4e894380c3d3cd342ae181af3d90178
unknown <priya.agri1999@gmail.com>
2021-07-31 05:42:53 -0700

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,6 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
 import "firebase/storage"
-const firebaseConfig = {
-    apiKey: "AIzaSyDHoWwB8rEsfc0-VC5f7a2H0pZh0urHSg0",
-    authDomain: "facebook-clone-35547.firebaseapp.com",
-    projectId: "facebook-clone-35547",
-    storageBucket: "facebook-clone-35547.appspot.com",
-    messagingSenderId: "109330018161",
-    appId: "1:109330018161:web:1caa6b0dfabea0adc590d0"
-  };
 
   const firebaseApp= firebase.initializeApp(firebaseConfig);
 
